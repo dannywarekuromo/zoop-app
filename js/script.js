97401cc085b8df00c0772890653f7e88ddcfdea0
dannywarekuromo <dannywarekuromo@gmail.com>
2023-11-29 14:05:02 +0100

    29-11-23

--- js/script.js
+++ js/script.js
@@ -13,9 +13,61 @@ window.addEventListener("load", () => {
     ".mobile-address-list-close"
   );
   const addressListLinks = document.querySelectorAll(".address-list-link");
+  const mobileAddressListLinks = document.querySelectorAll(".mobile-address-list-link");
   const curtain = document.querySelector(".curtain");
   const priceDropdown = document.querySelector(".restaurant-sort-dropdown-container");
 
+  /*=== display search history (desktop view)===*/
+  const appSearch = document.querySelector(".app-search")
+  const searchHistory = document.querySelector(".search-history")
+  const displaySearchHistory = (e) => {
+    appSearch.classList.add("active");
+    searchHistory.classList.add("active");
+    mobileNavLink.classList.remove("active");
+    mobileNavSearch.classList.add("active");
+  }
+
+  const removeSearchHistory = (e) => {
+    appSearch.classList.remove("active");
+    searchHistory.classList.remove("active"); 
+    mobileNavSearch.classList.remove("active");
+    mobileNavHome.classList.add("active");
+  }
+
+  appSearch.addEventListener("click", displaySearchHistory);
+  appSearch.addEventListener("submit", removeSearchHistory);
+
+
+  /*=== mobile search history  ===*/
+  const mobileAppSearch = document.querySelector(".mobile-app-search");
+  const mobileSearch = document.querySelector(".mobile-search");
+  const mobileNavSearch = document.querySelector(".mobile-nav-search");
+  const mobileNavHome = document.querySelector(".mobile-nav-home");
+  const mobileNavLink = document.querySelector(".mobile-nav-link");
+
+
+  /*=== display search query ===*/
+  const displaySearchQuery =(e) => {
+    mobileSearch.classList.add("active");
+    mobileNavLink.classList.remove("active");
+    mobileNavSearch.classList.add("active");
+  }
+
+  mobileAppSearch.addEventListener("click", displaySearchQuery );
+  mobileNavSearch.addEventListener("click", window.innerWidth > 800 ? displaySearchHistory : displaySearchQuery);
+
+   /*=== remove search query ===*/
+   const removeSearchQuery =(e) => {
+    mobileSearch.classList.remove("active");
+    mobileNavSearch.classList.remove("active");
+    mobileNavHome.classList.add("active");
+  }
+
+  mobileNavHome.addEventListener("click", window.innerWidth > 800 ? removeSearchHistory : removeSearchQuery);
+
+
+
+  /*=== display address list ===*/
   const displayAddressList = (e) => {
     const addressList = document.querySelector(".address-list");
     const mobileAddressList = document.querySelector(".mobile-address-list");
@@ -30,6 +82,49 @@ window.addEventListener("load", () => {
     }
   };
 
+  /*=== address list items ===*/
+
+  for (let i = 0; i < addressListLinks.length; i++) {
+    const addressName = document.querySelector(".address-name");
+    const addressParagraph = document.querySelector(".address-p");
+
+    addressListLinks[i].addEventListener("click", (e) => {
+      const addressListTitle = document.querySelectorAll(".address-list-title");
+      const addressLocation = document.querySelectorAll(".address-list-location");
+
+      const addressTitleText = addressListTitle[i].textContent;
+      const addressLocationText = addressLocation[i].textContent;
+
+      addressName.textContent = addressTitleText;
+      addressParagraph.textContent = addressLocationText;
+
+      removeAddressList();
+
+    })
+  }
+
+  /*=== mobile address list items ===*/
+  for (let i = 0; i < mobileAddressListLinks.length; i++) {
+    const addressName = document.querySelector(".address-name");
+    const addressParagraph = document.querySelector(".address-p");
+
+    mobileAddressListLinks[i].addEventListener("click", (e) => {
+      const mobileAddressListTitle = document.querySelectorAll(".mobile-address-list-title");
+      const mobileAddressLocation = document.querySelectorAll(".mobile-address-list-location");
+
+      const addressTitleText = mobileAddressListTitle[i].textContent;
+      const addressLocationText = mobileAddressLocation[i].textContent;
+
+      addressName.textContent = addressTitleText;
+      addressParagraph.textContent = addressLocationText;
+      // mobileAddressListLinks[i].classList.remove("active")
+      // mobileAddressListLinks[i].classList.add("active")
+      removeAddressList();
+
+    })
+  }
+
+  /*=== remove address list ===*/
   const removeAddressList = (e) => {
     const addressList = document.querySelector(".address-list");
     const mobileAddressList = document.querySelector(".mobile-address-list");
@@ -44,22 +139,15 @@ window.addEventListener("load", () => {
     }
   };
 
+  /*=== display dropdown menu ===*/
   const displayPriceDropdown = () => {
     priceDropdown.classList.toggle("active");
   }
 
   priceDropdown.addEventListener("click", displayPriceDropdown);
 
-  // storiesContainer.addEventListener("mouseenter", (e) => {
-  //   console.log(storiesContent.offsetWidth);
-  // })
-
   addressTitle.addEventListener("click", displayAddressList);
   addressCloseBtn.addEventListener("click", removeAddressList);
   mobileAddressCloseBtn.addEventListener("click", removeAddressList);
-  addressListLinks.forEach((addressLink) => {
-    addressLink.addEventListener("click", (e) => {
-      console.log(e);
-    });
-  });
+  
 });
